ff34e7c53aad3cffef8ad575e99e738142a9ceda
Arafat <kharkey05@gmail.com>
2025-04-17 20:12:04 +0600

    refactor: make CoinAcceptor implement PaymentAcceptor

--- src/vending_machine/CoinAcceptor.java
+++ src/vending_machine/CoinAcceptor.java
@@ -1,17 +1,28 @@
 package vending_machine;
 
-public class CoinAcceptor {
+public class CoinAcceptor implements PaymentAcceptor {
     private int amount;
 
     public CoinAcceptor(int amount) {
         this.amount = amount;
     }
 
+    @Override
     public int getAmount() {
         return amount;
     }
 
-    public void setAmount(int amount) {
-        this.amount = amount;
+    @Override
+    public void addAmount(int amount) {
+        this.amount += amount;
+    }
+
+    @Override
+    public boolean pay(int amount) {
+        if (this.amount >= amount) {
+            this.amount -= amount;
+            return true;
+        }
+        return false;
     }
 }
