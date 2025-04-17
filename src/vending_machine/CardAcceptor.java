2fa2733dbaf8f5e8016f31834523bd7b453134a8
Arafat <kharkey05@gmail.com>
2025-04-17 20:06:10 +0600

    Implement CardAcceptor for card-based payment

--- src/vending_machine/CardAcceptor.java
+++ src/vending_machine/CardAcceptor.java
@@ -1,4 +1,37 @@
 package vending_machine;
 
-public class CardAcceptor {
+import java.util.Scanner;
+
+public class CardAcceptor implements PaymentAcceptor {
+    private int balance;
+
+    public CardAcceptor(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public int getAmount() {
+        return balance;
+    }
+
+    @Override
+    public void addAmount(int amount) {
+        // Карта не пополняется вручную
+    }
+
+    @Override
+    public boolean pay(int amount) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите номер карты: ");
+        String card = scanner.nextLine();
+        System.out.print("Введите одноразовый код: ");
+        String otp = scanner.nextLine();
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println("Платеж успешен");
+            return true;
+        }
+        System.out.println("Недостаточно средств");
+        return false;
+    }
 }
